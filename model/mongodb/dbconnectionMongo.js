74aaa7b026aef4e55c2d777f9a4761a2c39bd8f1
Alexander kukushkin <kukushkinalex@gmail.com>
2024-03-06 15:28:59 +0200

    Readme and enviroment Updated

--- model/mongodb/dbconnectionMongo.js
+++ model/mongodb/dbconnectionMongo.js
@@ -2,19 +2,45 @@ import mongoose from "mongoose";
 import { conectedToMongo, NotConnectToMongo } from "../../utils/errors.js";
 
 const connectToMongo = () => {
-    return new Promise((resolve, reject) => {
-        mongoose
-            .connect(process.env.MONGODB_CONNECTION_STR + "ProjectName")
-            .then(() => {
-                conectedToMongo(); // Console error
-                resolve();
-            })
-            .catch((err) => {
-                NotConnectToMongo(); // Console error
-                reject(err);
-                process.exit(1); //drop server 
-            });
-    })
+    if (process.env.ENVIRONMENT === "production") {
+        return new Promise((resolve, reject) => {
+            mongoose
+                .connect(process.env.MONGODB_CONNECTION_STR + "bizdb")
+                .then(() => {
+                    console.log(chalk.magentaBright.bold("Connected to MongoDB"));
+                    resolve();
+                })
+                .catch((err) => {
+                    console.log(
+                        chalk.redBright.bold("Error connecting to MongoDB: ", err)
+                    );
+                    reject(err);
+                    process.exit(1);
+                });
+        });
+    }
+    if (process.env.ENVIRONMENT === "development") {
+        return new Promise((resolve, reject) => {
+            mongoose
+                .connect(process.env.REMOTE_URL + "bizdb")
+
+                .then(() => {
+                    console.log(
+                        chalk.magentaBright.bold(
+                            "Connected to MongoDB-LOCAL" + process.env.REMOTE_URL
+                        )
+                    );
+                    resolve();
+                })
+                .catch((err) => {
+                    console.log(
+                        chalk.redBright.bold("Error connecting to MongoDB: ", err)
+                    );
+                    reject(err);
+                    process.exit(1);
+                });
+        });
+    }
 };
 
 export default connectToMongo
